3ea1cf7d1eccfcbb8c90c83f932a6633c6b85c21
bruno <brunominighin@gmail.com>
2024-06-24 20:41:09 -0300

    theme button fixed

--- src/app/components/SecondButton.tsx
+++ src/app/components/SecondButton.tsx
@@ -11,7 +11,15 @@ function SecondButton({ content, style, onClick, appState }: ButtonProps) {
           : `border-[#00000028] shadow-lg shadow-[#00000010] text-[#000] hover:bg-[#d6d6d6] `
       } font-medium text-xs rounded-md transition-all`}
     >
-      {content === "" ? appState.mode ? <IoMoon className="h-4 w-4" /> : <IoSunny className="h-4 w-4" /> : content}
+      {content === "" ? (
+        !appState.mode ? (
+          <IoMoon className="h-4 w-4" />
+        ) : (
+          <IoSunny className="h-4 w-4" />
+        )
+      ) : (
+        content
+      )}
     </button>
   );
 }
